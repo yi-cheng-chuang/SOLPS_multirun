31cf98de883a4abad715de5f5bed8695260d1189
Yi-Cheng Chuang <ychuang@wm.edu>
2024-07-26 17:05:10 -0400

    Create modb2mn.py

---
+++ modb2mn.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 14:09:27 2024
+
+@author: ychuang
+"""
+
+import math
+import autorun_test as att
+import argparse
+
+parser = argparse.ArgumentParser(description='create multiple runs')
+parser.add_argument('-r','--runtype', type= str, metavar='', required=True, help='b2mn modify')
+args = parser.parse_args()
+
+"""
+
+run_type_list = ['longrun', 'ioutrun', 'testrun']
+
+"""
+
+
+
+if __name__ == '__main__':
+      
+    sim_dir = att.find_dir()
+    file_list = att.find_files(sim_dir= sim_dir)
+      
+    att.mod_multi_runtype(sim_dir = sim_dir, file_list = file_list, run_type = args.runtype)
+    
+    att.mod_runtype_and_play(sim_dir = sim_dir, file_list = file_list, run_type = args.runtype)
+    
