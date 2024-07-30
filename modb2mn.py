a701ecbe1e01e193ee9602f2877e542a056d3ea4
Yi-Cheng Chuang <ychuang@wm.edu>
2024-07-30 19:12:17 -0400

    Update modb2mn.py

--- modb2mn.py
+++ modb2mn.py
@@ -6,11 +6,12 @@ Created on Wed Jul 24 14:09:27 2024
 """
 
 import math
-import autorun_test as att
+import multirun as mr
 import argparse
 
 parser = argparse.ArgumentParser(description='create multiple runs')
 parser.add_argument('-r','--runtype', type= str, metavar='', required=True, help='b2mn modify')
+parser.add_argument('-t','--tail', type= str, metavar='', required=True, help='series run name tail')
 args = parser.parse_args()
 
 """
@@ -23,10 +24,10 @@ run_type_list = ['longrun', 'ioutrun', 'testrun']
 
 if __name__ == '__main__':
       
-    sim_dir = att.find_dir()
-    file_list = att.find_files(sim_dir= sim_dir)
+    sim_dir = mr.find_dir()
+    file_list = mr.find_files(sim_dir= sim_dir, tail= args.tail)
       
-    att.mod_multi_runtype(sim_dir = sim_dir, file_list = file_list, run_type = args.runtype)
+    mr.change_b2mn(sim_dir = sim_dir, file_list = file_list, run_type = args.runtype)
     
-    att.mod_runtype_and_play(sim_dir = sim_dir, file_list = file_list, run_type = args.runtype)
+    # mr.mod_b2mn_and_play(sim_dir = sim_dir, file_list = file_list, run_type = args.runtype)
     
