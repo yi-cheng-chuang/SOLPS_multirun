dc8b85f14b32fffd2933b751e845a75972142310
Yi-Cheng Chuang <ychuang@wm.edu>
2024-07-30 19:12:28 -0400

    Update squrescan_filecreation.py

--- squrescan_filecreation.py
+++ squrescan_filecreation.py
@@ -14,6 +14,8 @@ parser.add_argument('-t','--tail', type= str, metavar='',
                     required=True, help='series filename tail')
 parser.add_argument('-c','--copy', type= str, metavar='', 
                     required=True, help='copy folder')
+parser.add_argument('-s','--start', type= int, metavar='', 
+                    required=True, help='attampt file start number')
 args = parser.parse_args()
 
 """
@@ -26,8 +28,12 @@ tail =
 if __name__ == '__main__':
       
     sim_dir = mr.find_dir()
-      
-    mr.createfile_modboundary(sim_dir = sim_dir, tail = args.tail, copy_folder = args.copy)
+    
+    denscan_dic = {'start': 5.02, 'stop': 9.02, 'space': 5}
+    tempscan_dic = {'start': 3.73, 'stop': 7.73, 'space': 5}
+    
+    mr.sqscancpfile_modboundary(sim_dir = sim_dir, tail = args.tail, copy_folder = args.copy, 
+                        start_num = args.start, ds_dic = denscan_dic, ts_dic = tempscan_dic)
     
     file_list = mr.find_files(sim_dir= sim_dir, tail = args.tail)
       
