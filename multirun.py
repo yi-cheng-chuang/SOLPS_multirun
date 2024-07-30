01f4f3717a129c7aa1fc3eebc124f40c27ed76eb
Yi-Cheng Chuang <ychuang@wm.edu>
2024-07-30 19:12:23 -0400

    Update multirun.py

--- multirun.py
+++ multirun.py
@@ -7,7 +7,9 @@ Created on Thu Jul 11 17:09:42 2024
 
 #Import os module
 import os
+import re
 import file_modifier as fm
+import numpy as np
 
 
 """
@@ -117,7 +119,6 @@ def multi_plot_last10(sim_dir, file_list, last10_fname):
 
 
 
-
 def squrescan_filecreater(gen_folder, tail, shotnum_st, denscan_list, tempscan_list):
     
     
@@ -125,13 +126,173 @@ def squrescan_filecreater(gen_folder, tail, shotnum_st, denscan_list, tempscan_l
         
         for tem in tempscan_list:
         
-            new_folder = '{}_nf{:.1f}tf{:.1f}_{}'.format(shotnum_st, den, tem, tail)
+            new_folder = '{}_nf{:.2f}tf{:.2f}_{}'.format(shotnum_st, den, tem, tail)
             
             os.system('cp -r {} {}'.format(gen_folder, new_folder))
             
             shotnum_st = shotnum_st + 1
     
     
+
+def change_b2mn(sim_dir, file_list, run_type):
+    
+    for fname in file_list:
+        
+        if fname != 'baserun':
+            
+            case_loc = '{}/{}'.format(sim_dir, fname)
+            b2mn_loc = '{}/{}'.format(case_loc, 'b2mn.dat')
+                
+            os.chdir(case_loc)
+            
+            fm.b2mn_modifier(b2mn_loc = b2mn_loc, case_loc = case_loc, 
+                              run_type = run_type)
+        
+        else:
+            pass
+        
+            
+
+def change_batch(sim_dir, file_list, tail):
+    
+    for fname in file_list:
+        
+        if fname != 'baserun':
+            
+            case_loc = '{}/{}'.format(sim_dir, fname)
+            # b2mn_loc = '{}/{}'.format(case_loc, 'b2mn.dat')
+            batch_loc = '{}/{}'.format(case_loc, 'batch_example')
+            
+            # multifile_creater(gen_folder = gen_folder)
+            
+                     
+            if tail in fname:
+                
+                fm.batch_modifier(batch_loc = batch_loc, fname = fname, 
+                               case_loc = case_loc, idchange = True)
+
+
+def change_b2boundary(sim_dir, file_list, tail):
+    
+    
+    
+    for fname in file_list:
+        
+        case_loc = '{}/{}'.format(sim_dir, fname)
+        bbp_loc = '{}/{}'.format(case_loc, 'b2.boundary.parameters')
+        sp = fname.split('_')[1]
+        print(sp)
+        L = re.findall('\d+\.\d+', sp)
+        print(L)
+        bound_list = []
+        for ii in L:
+            bound_list.append(float(ii))
+        
+        print(bound_list)
+            
+            
+        fm.SN_b2boundary_modifier(b2boundary_loc = bbp_loc, case_loc = case_loc,
+                                      bound_list = bound_list)
+        
+
+            
+"""
+
+run_type_list = ['longrun', 'ioutrun', 'testrun']
+
+
+"""
+
+
+def mod_b2mn_and_play(sim_dir, file_list, run_type):
+    
+    for fname in file_list:
+        
+        if fname != 'baserun':
+            
+            case_loc = '{}/{}'.format(sim_dir, fname)
+            b2mn_loc = '{}/{}'.format(case_loc, 'b2mn.dat')
+                
+            os.chdir(case_loc)
+            
+            fm.b2mn_modifier(b2mn_loc = b2mn_loc, case_loc = case_loc, 
+                              run_type = run_type)
+            
+            os.system('cp b2fstate b2fstati')
+            os.system('rm b2mn.prt')
+            os.system('qsub batch_example')
+        
+        else:
+            pass
+
+
+
+def sqscancpfile_modboundary(sim_dir, tail, copy_folder, start_num, ds_dic, ts_dic):
+    
+    
+    ds_start_num = ds_dic['start']
+    ds_stop_num = ds_dic['stop']
+    ds_space_num = ds_dic['space']
+    
+    ds_list = np.linspace(ds_start_num, ds_stop_num, ds_space_num)
+    
+    ts_start_num = ts_dic['start']
+    ts_stop_num = ts_dic['stop']
+    ts_space_num = ts_dic['space']
+    
+    ts_list = np.linspace(ts_start_num, ts_stop_num, ts_space_num)
+    
+    
+    squrescan_filecreater(gen_folder = copy_folder, tail = tail, 
+                        shotnum_st = start_num, denscan_list = ds_list, 
+                        tempscan_list = ts_list)
+    
+
+
+
+def remove_file(file_list):
+    
+    
+    for fname in file_list:
+        
+            
+        os.system('rm -r {}'.format(fname))
+        print('{} is removed'.format(fname))
+    
+
+def create_b2fplasmf_output(sim_dir, file_list):
+    
+    for fname in file_list:
+                 
+        case_loc = '{}/{}'.format(sim_dir, fname)
+            
+        os.chdir(case_loc)
+        
+        os.system('b2run b2uf')
+        os.system('pwd')
+        os.system('OutputGen')
+        os.system('pwd')
+        os.system('EirOutputGen')
+    
+
+
+
+
+"""
+
+#Create a new directory
+os.mkdir("test_folder")
+
+#Change current working directory
+os.chdir("test_folder")
+
+#Print current working directory
+print("Current directory now:" , os.getcwd())
+
+
+os.system('cp b2fstate b2fstati')
+os.system('rm b2mn.prt')
+
 
 def multi_run(sim_dir, file_list, scenario_type):
     
@@ -188,145 +349,7 @@ def multi_run(sim_dir, file_list, scenario_type):
                 pass
 
 
-def mod_multi_runtype(sim_dir, file_list, run_type):
-    
-    for fname in file_list:
-        
-        if fname != 'baserun':
-            
-            case_loc = '{}/{}'.format(sim_dir, fname)
-            b2mn_loc = '{}/{}'.format(case_loc, 'b2mn.dat')
-                
-            os.chdir(case_loc)
-            
-            fm.b2mn_modifier(b2mn_loc = b2mn_loc, case_loc = case_loc, 
-                              run_type = run_type)
-        
-        else:
-            pass
-        
-            
 
-def change_batch(sim_dir, file_list, tail):
-    
-    for fname in file_list:
-        
-        if fname != 'baserun':
-            
-            case_loc = '{}/{}'.format(sim_dir, fname)
-            # b2mn_loc = '{}/{}'.format(case_loc, 'b2mn.dat')
-            batch_loc = '{}/{}'.format(case_loc, 'batch_example')
-            
-            # multifile_creater(gen_folder = gen_folder)
-            
-                     
-            if tail in fname:
-                
-                fm.batch_modifier(batch_loc = batch_loc, fname = fname, 
-                               case_loc = case_loc, idchange = True)
-
-
-def change_b2boundary(sim_dir, file_list, tail):
-    
-    
-    
-    for fname in file_list:
-        
-        if fname != 'baserun':
-            
-            case_loc = '{}/{}'.format(sim_dir, fname)
-            bbp_loc = '{}/{}'.format(case_loc, 'b2.boundary.parameter')
-                       
-                     
-            if tail in fname:
-                
-                fm.SN_b2boundary_modifier(b2boundary_loc = bbp_loc, case_loc = case_loc)
-
-
-
-
-
-
-
-"""
-
-run_type_list = ['longrun', 'ioutrun', 'testrun']
-
-
-
-"""
-
-
-def mod_runtype_and_play(sim_dir, file_list, run_type):
-    
-    for fname in file_list:
-        
-        if fname != 'baserun':
-            
-            case_loc = '{}/{}'.format(sim_dir, fname)
-            b2mn_loc = '{}/{}'.format(case_loc, 'b2mn.dat')
-                
-            os.chdir(case_loc)
-            
-            fm.b2mn_modifier(b2mn_loc = b2mn_loc, case_loc = case_loc, 
-                              run_type = run_type)
-            
-            os.system('cp b2fstate b2fstati')
-            os.system('rm b2mn.prt')
-            os.system('qsub batch_example')
-        
-        else:
-            pass
-
-
-
-def squrescan_createfile(sim_dir, tail, copy_folder):
-    
-    st_num = 76
-    ds_list = [3.4, 4.0, 5.0, 6.0, 7.0]
-    ts_list = [2.5, 3.0, 4.0, 5.0, 6.0]
-    
-    squrescan_filecreater(gen_folder = copy_folder, tail = tail, 
-                        shotnum_st = st_num, denscan_list = ds_list, 
-                        tempscan_list = ts_list)
-    
-
-
-
-
-
-
-
-def remove_file(file_list):
-    
-    
-    for fname in file_list:
-        
-            
-        os.system('rm -r {}'.format(fname))
-        print('{} is removed'.format(fname))
-    
-
-
-    
-
-
-
-
-"""
-
-#Create a new directory
-os.mkdir("test_folder")
-
-#Change current working directory
-os.chdir("test_folder")
-
-#Print current working directory
-print("Current directory now:" , os.getcwd())
-
-
-os.system('cp b2fstate b2fstati')
-os.system('rm b2mn.prt')
 
 """
 
