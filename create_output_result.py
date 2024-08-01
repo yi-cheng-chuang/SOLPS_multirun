1287ede4de541e5e72ff6e37bcf5c95b67d696b8
Yi-Cheng Chuang <ychuang@wm.edu>
2024-08-01 13:10:03 -0400

    Update create_output_result.py

--- create_output_result.py
+++ create_output_result.py
@@ -11,6 +11,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='create multiple runs')
 parser.add_argument('-t','--tail', type= str, metavar='', required=True, help='series run name tail')
+parser.add_argument('-sh','--shot', type= str, metavar='', required=True, help='discharge number')
 args = parser.parse_args()
 
 """
@@ -26,6 +27,6 @@ if __name__ == '__main__':
     sim_dir = mr.find_dir()
     file_list = mr.find_files(sim_dir= sim_dir, tail= args.tail)
       
-    mr.create_output(sim_dir = sim_dir, file_list = file_list)
+    mr.create_output(sim_dir = sim_dir, file_list = file_list, shot_num = args.shot)
     
     # mr.mod_b2mn_and_play(sim_dir = sim_dir, file_list = file_list, run_type = args.runtype)
