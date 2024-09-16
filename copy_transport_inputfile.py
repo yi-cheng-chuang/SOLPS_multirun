93bcc733ce2f7365ea33d6927eace6abc767fe8a
Yi-Cheng Chuang <ychuang@wm.edu>
2024-09-16 17:10:14 -0400

    Create copy_transport_inputfile.py

---
+++ copy_transport_inputfile.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 11:00:45 2024
+
+@author: ychuang
+"""
+
+import math
+import multirun as mr
+import argparse
+
+parser = argparse.ArgumentParser(description='create multiple runs')
+parser.add_argument('-t','--tail', type= str, metavar='', 
+                    required=True, help='series filename tail')
+args = parser.parse_args()
+
+
+
+
+if __name__ == '__main__':
+      
+    sim_dir = mr.find_dir()
+    file_list = mr.find_files(sim_dir= sim_dir, tail = args.tail)
+      
+    mr.multi_transportinput_copy(sim_dir = sim_dir, file_list = file_list)
