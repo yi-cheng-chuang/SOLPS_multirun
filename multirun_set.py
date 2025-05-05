4ebda2b2a2e3d0337faf37419e5a7afe7aa2d07a
Yi-Cheng Chuang <ychuang@wm.edu>
2025-05-05 11:52:39 -0400

    Update multirun_set.py

--- multirun_set.py
+++ multirun_set.py
@@ -28,7 +28,7 @@ def b2mn_set(run_type):
     elif run_type == 'testrun':
         
         
-        b2mn_basicrunvalue_dic = {'b2mndr_ntim': '502', 'b2mndr_dtim': '6.0e-5', 'b2mndr_stim': '-1.0'}
+        b2mn_basicrunvalue_dic = {'b2mndr_ntim': '20', 'b2mndr_dtim': '6.0e-5', 'b2mndr_stim': '-1.0'}
 
     
     else:
